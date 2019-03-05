3d2f0c2f583ea1fc23027c0903c2168158381b09
Jordi Vidal <jordividal1997@gmail.com>
2019-03-05 17:03:24 +0100

    work

--- config.php
+++ config.php
@@ -4,7 +4,7 @@
         'host' => 'localhost',
         'username' => 'root',
         'password' => '',
-        'db' => 'blog' //Cambiar al nombre de tu base de datos
+        'db' => 'biblioteca' 
     ];
 
     ?>
